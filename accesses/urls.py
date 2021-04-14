64503845e18f07a59ae8eaf2022d3da3256f04c0
sayxyoung <hyjlove11@gmail.com>
2021-04-14 16:31:19 +0900

    Add : add DoorUseLogDetailListAV in accesses
    add DoorUseLogDetailListAV using the APIView

--- accesses/urls.py
+++ accesses/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import DoorUseLogListGV
+from .views import (
+    DoorUseLogListGV, DoorUseLogDetailListAV,
+)
 
 urlpatterns = [
     path('/access', DoorUseLogListGV.as_view(), name='access-list'),
+    path('/access/<str:generation>', DoorUseLogDetailListAV.as_view(), name='access-detail'),
 ]
--- accesses/views.py
+++ accesses/views.py
@@ -1,7 +1,11 @@
 from rest_framework             import generics
 from rest_framework.permissions import IsAdminUser
+from rest_framework.response    import Response
+from rest_framework.views       import APIView
+from rest_framework.views       import status
 
 from .models         import DoorUseLog
+from .permissions    import IsAdminOrGeneration
 from .serializers    import DoorUseLogSerializer
 
 
@@ -9,3 +13,15 @@ class DoorUseLogListGV(generics.ListAPIView):
     queryset           = DoorUseLog.objects.all()
     serializer_class   = DoorUseLogSerializer
     permission_classes = [IsAdminUser]
+
+
+class DoorUseLogDetailListAV(APIView):
+    permission_classes = [IsAdminOrGeneration]
+
+    def get(self, request, generation):
+        dooruselogs = DoorUseLog.objects.filter(generation__name=generation)
+        if not dooruselogs:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        self.check_object_permissions(self.request, dooruselogs[0])
+        serializer = DoorUseLogSerializer(dooruselogs, many=True)
+        return Response(serializer.data)
